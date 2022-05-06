e77ccef9d3812ebe91fa43c27bc920aa6ad6d10a
Nabu_the_Great <kiran97@criptext.com>
2022-05-06 14:41:24 +0530

    second

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -4,24 +4,25 @@ const math=require('math-expression-evaluator')
 // create and save new user
 exports.create = (req,res)=>{
     // validate request
-    if(!req.body){
-        res.status(400).send({ message : "Content can not be emtpy!"});
-        return;
-    }
+    // if(!req.body){
+    //     res.status(400).send({ message : "Content can not be emtpy!"});
+    //     return;
+    // }
 
     // new user
     const user = new Userdb({
         name : req.body.name
         // email : req.body.email,
         // gender: req.body.gender,
-        // status : req.body.status
+        
     })
-    console.log(user.name)
-    if ((user.name).match(/[A-Z]/gi)){
+    
+    if ((user.name).match(/[A-Z]/gi) ){
         user
         .save(user)
         .then(data => {
             //res.send(data)
+           
             res.redirect('/');
         })
         .catch(err =>{
